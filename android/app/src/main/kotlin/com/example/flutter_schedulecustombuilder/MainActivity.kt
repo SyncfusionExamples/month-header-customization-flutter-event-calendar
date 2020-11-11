74dda60cfc75eb32a9f0164e8506f3e54ca3324d
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-11-11 18:08:59 +0530

    FLUT-2624 - Sample and screenshot added

---
+++ android/app/src/main/kotlin/com/example/flutter_schedulecustombuilder/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_schedulecustombuilder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+void main() => runApp(ScheduleViewBuilder());
+
+class ScheduleViewBuilder extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            body: SafeArea(
+          child: SfCalendar(
+            view: CalendarView.schedule,
+            dataSource: _getDataSource(),
+            scheduleViewMonthHeaderBuilder: scheduleViewHeaderBuilder,
+          ),
+        )));
+  }
+
+  _DataSource _getDataSource() {
+    final List<Appointment> appointments = <Appointment>[];
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(hours: 4)),
+      endTime: DateTime.now().add(Duration(hours: 5)),
+      subject: 'Meeting',
+      color: Colors.red,
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(days: -2, hours: 4)),
+      endTime: DateTime.now().add(Duration(days: -2, hours: 5)),
+      subject: 'Development Meeting   New York, U.S.A',
+      color: Color(0xFFf527318),
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(days: -2, hours: 3)),
+      endTime: DateTime.now().add(Duration(days: -2, hours: 4)),
+      subject: 'Project Plan Meeting   Kuala Lumpur, Malaysia',
+      color: Color(0xFFfb21f66),
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(days: -2, hours: 2)),
+      endTime: DateTime.now().add(Duration(days: -2, hours: 3)),
+      subject: 'Support - Web Meeting   Dubai, UAE',
+      color: Color(0xFFf3282b8),
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(days: -2, hours: 1)),
+      endTime: DateTime.now().add(Duration(days: -2, hours: 2)),
+      subject: 'Project Release Meeting   Istanbul, Turkey',
+      color: Color(0xFFf2a7886),
+    ));
+    appointments.add(Appointment(
+        startTime: DateTime.now().add(const Duration(hours: 4, days: -1)),
+        endTime: DateTime.now().add(const Duration(hours: 5, days: -1)),
+        subject: 'Release Meeting',
+        color: Colors.lightBlueAccent,
+        isAllDay: true));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(const Duration(hours: 2, days: -4)),
+      endTime: DateTime.now().add(const Duration(hours: 4, days: -4)),
+      subject: 'Performance check',
+      color: Colors.amber,
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(const Duration(hours: 11, days: -2)),
+      endTime: DateTime.now().add(const Duration(hours: 12, days: -2)),
+      subject: 'Customer Meeting   Tokyo, Japan',
+      color: Color(0xFFffb8d62),
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(const Duration(hours: 6, days: 2)),
+      endTime: DateTime.now().add(const Duration(hours: 7, days: 2)),
+      subject: 'Retrospective',
+      color: Colors.purple,
+    ));
+
+    return _DataSource(appointments);
+  }
+
+  Widget scheduleViewHeaderBuilder(
+      BuildContext buildContext, ScheduleViewMonthHeaderDetails details) {
+    final String monthName = _getMonthName(details.date.month);
+    return Stack(
+      children: [
+        Image(
+            image: ExactAssetImage('images/' + monthName + '.png'),
+            fit: BoxFit.cover,
+            width: details.bounds.width,
+            height: details.bounds.height),
+        Positioned(
+          left: 55,
+          right: 0,
+          top: 20,
+          bottom: 0,
+          child: Text(
+            monthName + ' ' + details.date.year.toString(),
+            style: TextStyle(fontSize: 18),
+          ),
+        )
+      ],
+    );
+  }
+
+  String _getMonthName(int month) {
+    if (month == 01) {
+      return 'January';
+    } else if (month == 02) {
+      return 'February';
+    } else if (month == 03) {
+      return 'March';
+    } else if (month == 04) {
+      return 'April';
+    } else if (month == 05) {
+      return 'May';
+    } else if (month == 06) {
+      return 'June';
+    } else if (month == 07) {
+      return 'July';
+    } else if (month == 08) {
+      return 'August';
+    } else if (month == 09) {
+      return 'September';
+    } else if (month == 10) {
+      return 'October';
+    } else if (month == 11) {
+      return 'November';
+    } else {
+      return 'December';
+    }
+  }
+}
+
+class _DataSource extends CalendarDataSource {
+  _DataSource(List<Appointment> source) {
+    appointments = source;
+  }
+}

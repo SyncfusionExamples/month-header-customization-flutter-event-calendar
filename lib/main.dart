d6c6fa5dfdbbb3344d3ddc56edcf6eb164b4d5e5
Muniappan <muniappan.subramanian@syncfusiopn.com>
2022-12-09 16:29:31 +0530

    sample updated

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 
-void main() => runApp(ScheduleViewBuilder());
+void main() => runApp(const ScheduleViewBuilder());
 
 class ScheduleViewBuilder extends StatelessWidget {
+  const ScheduleViewBuilder({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,34 +24,34 @@ class ScheduleViewBuilder extends StatelessWidget {
   _DataSource _getDataSource() {
     final List<Appointment> appointments = <Appointment>[];
     appointments.add(Appointment(
-      startTime: DateTime.now().add(Duration(hours: 4)),
-      endTime: DateTime.now().add(Duration(hours: 5)),
+      startTime: DateTime.now().add(const Duration(hours: 4)),
+      endTime: DateTime.now().add(const Duration(hours: 5)),
       subject: 'Meeting',
       color: Colors.red,
     ));
     appointments.add(Appointment(
-      startTime: DateTime.now().add(Duration(days: -2, hours: 4)),
-      endTime: DateTime.now().add(Duration(days: -2, hours: 5)),
+      startTime: DateTime.now().add(const Duration(days: -2, hours: 4)),
+      endTime: DateTime.now().add(const Duration(days: -2, hours: 5)),
       subject: 'Development Meeting   New York, U.S.A',
-      color: Color(0xFFf527318),
+      color: const Color(0xFFf527318),
     ));
     appointments.add(Appointment(
-      startTime: DateTime.now().add(Duration(days: -2, hours: 3)),
-      endTime: DateTime.now().add(Duration(days: -2, hours: 4)),
+      startTime: DateTime.now().add(const Duration(days: -2, hours: 3)),
+      endTime: DateTime.now().add(const Duration(days: -2, hours: 4)),
       subject: 'Project Plan Meeting   Kuala Lumpur, Malaysia',
-      color: Color(0xFFfb21f66),
+      color: const Color(0xFFfb21f66),
     ));
     appointments.add(Appointment(
-      startTime: DateTime.now().add(Duration(days: -2, hours: 2)),
-      endTime: DateTime.now().add(Duration(days: -2, hours: 3)),
+      startTime: DateTime.now().add(const Duration(days: -2, hours: 2)),
+      endTime: DateTime.now().add(const Duration(days: -2, hours: 3)),
       subject: 'Support - Web Meeting   Dubai, UAE',
-      color: Color(0xFFf3282b8),
+      color: const Color(0xFFf3282b8),
     ));
     appointments.add(Appointment(
-      startTime: DateTime.now().add(Duration(days: -2, hours: 1)),
-      endTime: DateTime.now().add(Duration(days: -2, hours: 2)),
+      startTime: DateTime.now().add(const Duration(days: -2, hours: 1)),
+      endTime: DateTime.now().add(const Duration(days: -2, hours: 2)),
       subject: 'Project Release Meeting   Istanbul, Turkey',
-      color: Color(0xFFf2a7886),
+      color: const Color(0xFFf2a7886),
     ));
     appointments.add(Appointment(
         startTime: DateTime.now().add(const Duration(hours: 4, days: -1)),
@@ -67,7 +69,7 @@ class ScheduleViewBuilder extends StatelessWidget {
       startTime: DateTime.now().add(const Duration(hours: 11, days: -2)),
       endTime: DateTime.now().add(const Duration(hours: 12, days: -2)),
       subject: 'Customer Meeting   Tokyo, Japan',
-      color: Color(0xFFffb8d62),
+      color: const Color(0xFFffb8d62),
     ));
     appointments.add(Appointment(
       startTime: DateTime.now().add(const Duration(hours: 6, days: 2)),
@@ -96,7 +98,7 @@ class ScheduleViewBuilder extends StatelessWidget {
           bottom: 0,
           child: Text(
             monthName + ' ' + details.date.year.toString(),
-            style: TextStyle(fontSize: 18),
+            style: const TextStyle(fontSize: 18),
           ),
         )
       ],
